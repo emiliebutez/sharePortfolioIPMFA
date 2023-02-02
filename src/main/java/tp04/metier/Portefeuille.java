030da7aa560af37eadc94adcb97cd5ba17c257b3
Louis-Lcq <83230080+Louis-Lcq@users.noreply.github.com>
2023-02-02 10:25:21 +0100

    1) Commit US 2-3 Louis cop Melissa Declaration variables dans test pour les fonctions acheter/vendre
    2)
    Commit US 2-3 Melissa cop Louis
    Start ecriture commentaires sur PortefeuilleTest, et Portefeuille,
    Complemention pour les tests des US 2 & 3 Et Run --> OK
    
    3)
    Commit US 2-3 Louis cop Melissa
    TU Acheter ( amelioration possible pour les quantités négatives)
    
    4)
    Commit US 2-3 Melissa cop Louis
    TU Acheter avec quantités négatives
    
    5)
    Commit US 2-3 Louis cop Melissa
    TU Vendre le même nombre de quantité que celles achetées
    
    6)
    Commit US 2-3 Louis cop Melissa
    TU Acheter not pass
    
    7)
    Commit US 2-3 Melissa cop Louis
    Commentaires portefeuilleTest et portefeuille

--- src/main/java/tp04/metier/Portefeuille.java
+++ src/main/java/tp04/metier/Portefeuille.java
@@ -13,78 +13,103 @@ import java.util.Map;
  * @author perussel
  */
 public class Portefeuille {
-    
-    Map<Action, LignePortefeuille> mapLignes;
-    
-    private class LignePortefeuille {
-        
-        private Action action;
-        
-        private int qte;
-        
-        public int getQte() {
-            return qte;
-        }
-        
-        public void setQte(int qte) {
-            this.qte = qte;
-        }
-        
-        public Action getAction() {
-            return this.action;
-        }
-        
-        public LignePortefeuille(Action action, int qte) {
-            this.action = action;
-            this.qte = qte;
-        }
+  //Liste des action du portefeuille
 
-        public String toString() {
-            return Integer.toString(qte);
-        }
-    }
-    
-    public Portefeuille() {
-        this.mapLignes = new HashMap();
-    }
-    
-    public void acheter(Action a, int q) {
-        if (this.mapLignes.containsKey(a) == false) {
-            this.mapLignes.put(a, new LignePortefeuille(a, q));
-        } else {
-            this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() + q);
-        }
+  Map<Action, LignePortefeuille> mapLignes;
+
+  private class LignePortefeuille {
+    //Parametres de la classe LignePortefeuille
+
+    private Action action;
+
+    private int qte;
+    // Methodes GET & SET 
+
+    public int getQte() {
+      return qte;
     }
 
-    public void vendre(Action a, int q) {
-        if (this.mapLignes.containsKey(a) == true) {
-            if (this.mapLignes.get(a).getQte() > q) {
-                this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() - q);
-            } else if (this.mapLignes.get(a).getQte() == q) {
-                this.mapLignes.remove(a);
-            }
-        }        
+    public void setQte(int qte) {
+      this.qte = qte;
     }
-    
+
+    public Action getAction() {
+      return this.action;
+    }
+    //Constructeur de la classe LignePortefeuille
+
+    public LignePortefeuille(Action action, int qte) {
+      this.action = action;
+      this.qte = qte;
+    }
+    //Methode toString() 
+
     public String toString() {
-        return this.mapLignes.toString();
+      return Integer.toString(qte);
     }
+  }
+  //Methode de la classe Portefeuille
 
-    public float valeur(Jour j) {
-        float total = 0;
-        for (LignePortefeuille lp : this.mapLignes.values()) {
-            total = total + (lp.getQte() * lp.getAction().valeur(j));
-        }
-        return total;
+  public Portefeuille() {
+    this.mapLignes = new HashMap();
+  }
+
+  //Methode d'achat d'une action
+  public void acheter(Action a, int q) {
+    if (q <= 0)
+    {
+      System.out.println("on ne peut pas acheter de quantités négatives");
+    } else if (this.mapLignes.containsKey(a) == false)
+    {
+      this.mapLignes.put(a, new LignePortefeuille(a, q));
+    } else
+    {
+      this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() + q);
     }
-    
-    
-    /** retoune le Montant total des valeur des actions du porte feuille */
-    public float getMontantPF(Jour j) {
-      float valeur = 0;
-      for (Map.Entry<Action,LignePortefeuille> mapEntry: mapLignes.entrySet()) {
-        System.out.println("Action:"+mapEntry.getKey().getLibelle()+"qte:"+mapEntry.getValue().getQte()+"valeur"+mapEntry.getValue().getAction().getCours(j));
-         valeur = valeur + mapEntry.getValue().getAction().getCours(j);
-      }    
+  }
+
+  // méthode permettant de vendre une action
+  public void vendre(Action a, int q) {
+    if (this.mapLignes.containsKey(a) == true)
+    {
+      if (this.mapLignes.get(a).getQte() > q)
+      {
+        this.mapLignes.get(a).setQte(this.mapLignes.get(a).getQte() - q);
+      } else if (this.mapLignes.get(a).getQte() == q)
+      {
+        this.mapLignes.remove(a);
+      } else
+      {
+        System.out.println("on ne peut pas vendre plus que la quantité achetée");
+      }
+    }
+  }
+  //Methode toString()
+
+  public String toString() {
+    return this.mapLignes.toString();
+  }
+
+  //Methode qui permet d'obtenir la valeur du portefeuille à un jour donnée
+  public float valeur(Jour j) {
+    float total = 0;
+    for (LignePortefeuille lp : this.mapLignes.values())
+    {
+      total = total + (lp.getQte() * lp.getAction().valeur(j));
+    }
+    return total;
+  }
+
+  /**
+   * retoune le Montant total des valeur des actions du porte feuille
+   */
+  public float getMontantPF(Jour j) {
+    float valeur = 0;
+    for (Map.Entry<Action, LignePortefeuille> mapEntry : mapLignes.entrySet())
+    {
+      System.out.println("Action:" + mapEntry.getKey().getLibelle() + "qte:" + mapEntry.getValue().getQte() + "valeur" + mapEntry.getValue().getAction().getCours(j));
+      valeur = valeur + mapEntry.getValue().getAction().getCours(j);
+    }
     return valeur;
-    }}
+  }
+}
---
+++ src/test/java/tp04/metier/PortefeuilleTest.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2023 louis.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tp04.metier;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author louis
+ */
+public class PortefeuilleTest {
+
+  public PortefeuilleTest() {
+  }
+
+  @Test
+  public void testSomeMethod() {
+  }
+
+  /**
+   * Acheter une quantité
+   */
+  @Test
+  public void AcheterShouldPass() {
+    Jour j1 = new Jour(2023, 20);
+    Portefeuille p1 = new Portefeuille();
+    ActionSimple a1 = new ActionSimple("Apple");
+    a1.enrgCours(j1, 5f);
+    p1.acheter(a1, 1);
+
+    assertEquals(5, p1.valeur(j1));
+  }
+
+  /**
+   * Acheter une quantité négative
+   */
+  @Test
+  public void AcheterShouldNotPass() {
+    Jour j1 = new Jour(2023, 20);
+    Portefeuille p1 = new Portefeuille();
+    ActionSimple a1 = new ActionSimple("Apple");
+    a1.enrgCours(j1, 5f);
+    p1.acheter(a1, -10);
+    assertNotEquals(-50, p1.valeur(j1));
+  }
+
+  /**
+   * Vendre une quantité égale à celle qui est possédée
+   */
+  @Test
+  public void VendreEqualsShouldPass() {
+    Jour j1 = new Jour(2023, 20);
+    Portefeuille p1 = new Portefeuille();
+    ActionSimple a1 = new ActionSimple("Apple");
+    a1.enrgCours(j1, 5f);
+    p1.acheter(a1, 1);
+    p1.vendre(a1, 1);
+
+    assertEquals(0, p1.valeur(j1));
+  }
+
+  /**
+   * Vendre une quantité supérieure à celle qui est possédée
+   */
+  @Test
+  public void VendreShouldNotPass() {
+    Jour j1 = new Jour(2023, 20);
+    Portefeuille p1 = new Portefeuille();
+    ActionSimple a1 = new ActionSimple("Apple");
+    a1.enrgCours(j1, 5f);
+    p1.acheter(a1, 1);
+    p1.vendre(a1, 2);
+
+    assertNotEquals(-5, p1.valeur(j1));
+  }
+
+  /**
+   * Vendre une quantité inférieure à celle qui est possédée
+   */
+  @Test
+  public void VendreShouldPass() {
+    Jour j1 = new Jour(2023, 20);
+    Portefeuille p1 = new Portefeuille();
+    ActionSimple a1 = new ActionSimple("Apple");
+    a1.enrgCours(j1, 5f);
+    p1.acheter(a1, 2);
+    p1.vendre(a1, 1);
+
+    assertEquals(5, p1.valeur(j1));
+  }
+}
