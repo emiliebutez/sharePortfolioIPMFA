cbdd73d89897ba1b2d7e0756b1c9c33e7be8b7c6
emiliebutez <emiliebutez.eb@gmail.com>
2023-02-03 09:20:58 +0100

    US06 Correction de warning.
    Pilote : Emilie
    Co-pilote : Chad

--- src/test/java/tp04/metier/InvestisseurTest.java
+++ src/test/java/tp04/metier/InvestisseurTest.java
@@ -13,70 +13,112 @@ import org.junit.jupiter.api.Test;
  * @author Emilie
  */
 public class InvestisseurTest {
+  /**
+   * Le code attendu.
+   */
+  private static final int EXPECTED_CODE = 1;
+  /**
+   * Le nom attendu.
+   */
+  private static final String EXPECTED_NOM = "nomTest";
+  /**
+   * Le prenom attendu.
+   */
+  private static final String EXPECTED_PRENOM = "prenomTest";
+  /**
+   * L'email attendu.
+   */
+  private static final String EXPECTED_EMAIL = "mailTest";
+  /**
+   * Le code non attendu.
+   */
+  private static final int NO_EXPECTED_CODE = 2;
+  /**
+   * Le nom non attendu.
+   */
+  private static final String NO_EXPECTED_NOM = "nomFaux";
+  /**
+   * Le prenom non attendu.
+   */
+  private static final String NO_EXPECTED_PRENOM = "prenomFaux";
+  /**
+   * L'email non attendu.
+   */
+  private static final String NO_EXPECTED_EMAIL = "mailFaux";
 
+  /**
+   * Constructeur de la classe InvestisseurTest.
+   */
   public InvestisseurTest() {
   }
 
-  private static final int EXPECTED_CODE = 1;
-  private static final String EXPECTED_NOM = "nomTest";
-  private static final String EXPECTED_PRENOM = "prenomTest";
-  private static final String EXPECTED_EMAIL = "mailTest";
-  private static final int NO_EXPECTED_CODE = 2;
-  private static final String NO_EXPECTED_NOM = "nomFaux";
-  private static final String NO_EXPECTED_PRENOM = "prenomFaux";
-  private static final String NO_EXPECTED_EMAIL = "mailFaux";
-
+  /**
+   * Test de la méthode getCodeI.
+   */
   @Test
-  public void testGetCodeShouldPass() {
-    final Investisseur testI = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+  final void testGetCodeShouldPass() {
+    final Investisseur testI = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
     final int result = testI.getCodeI();
 
     Assertions.assertSame(EXPECTED_CODE,
             result,
             "Le codeI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la méthode getNomI.
+   */
   @Test
-  public void testGetNomShouldPass() {
-    final Investisseur testI = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+  final void testGetNomShouldPass() {
+    final Investisseur testI = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
     final String result = testI.getNomI();
 
     Assertions.assertSame(EXPECTED_NOM,
             result,
             "Le nomI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la méthode getPrenomI.
+   */
   @Test
-  public void testGetPrenomShouldPass() {
-    final Investisseur testI = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+  final void testGetPrenomShouldPass() {
+    final Investisseur testI = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
     final String result = testI.getPrenomI();
 
     Assertions.assertSame(EXPECTED_PRENOM,
             result,
             "Le prenomI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la methode getEmailI.
+   */
   @Test
-  public void testGetEmailShouldPass() {
-    final Investisseur testI = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+  final void testGetEmailShouldPass() {
+    final Investisseur testI = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
     final String result = testI.getEmailI();
 
     Assertions.assertSame(EXPECTED_EMAIL,
             result,
             "Le mailI doit etre le meme que result"
     );
-
   }
 
-  @Test
-  public void testSetCodeShouldPass() {
-    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE, NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
+  /**
+   * Test de la méthode setCodeI.
+   */
+   @Test
+  final void testSetCodeShouldPass() {
+    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE,
+            NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
     testI.setCodeI(EXPECTED_CODE);
     final int result = testI.getCodeI();
 
@@ -84,12 +126,15 @@ public class InvestisseurTest {
             result,
             "Le codeI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la méthode setNomI.
+   */
   @Test
-  public void testSetNomShouldPass() {
-    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE, NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
+  final void testSetNomShouldPass() {
+    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE,
+            NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
     testI.setNomI(EXPECTED_NOM);
     final String result = testI.getNomI();
 
@@ -97,12 +142,15 @@ public class InvestisseurTest {
             result,
             "Le nomI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la méthode setPrenom.
+   */
   @Test
-  public void testSetPrenomShouldPass() {
-    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE, NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
+  final void testSetPrenomShouldPass() {
+    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE,
+            NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
     testI.setPrenomI(EXPECTED_PRENOM);
     final String result = testI.getPrenomI();
 
@@ -110,12 +158,15 @@ public class InvestisseurTest {
             result,
             "Le nomI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la méthode setEmailI.
+   */
   @Test
-  public void testSetEmailShouldPass() {
-    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE, NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
+  final void testSetEmailShouldPass() {
+    final Investisseur testI = new Investisseur(NO_EXPECTED_CODE,
+            NO_EXPECTED_NOM, NO_EXPECTED_PRENOM, NO_EXPECTED_EMAIL);
     testI.setEmailI(EXPECTED_EMAIL);
     final String result = testI.getEmailI();
 
@@ -123,27 +174,35 @@ public class InvestisseurTest {
             result,
             "Le mailI doit etre le meme que result"
     );
-
   }
 
+  /**
+   * Test de la méthode equals.
+   */
   @Test
-  public void testEquals() {
-    final Investisseur testI1 = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
-    final Investisseur testI2 = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
-
+  final void testEquals() {
+    final Investisseur testI1 = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+    final Investisseur testI2 = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+    
     boolean equal = testI1.equals(testI2);
     Assertions.assertTrue(equal);
     Assertions.assertTrue(testI1.hashCode() == testI2.hashCode());
   }
 
-  public void toStringTest() {
-    final Investisseur testI = new Investisseur(EXPECTED_CODE, EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
-    Assertions.assertEquals("Investisseur{"
-            + "codeI=" + EXPECTED_CODE
-            + ", nomI='" + EXPECTED_NOM + '\''
-            + ", prenomI='" + EXPECTED_PRENOM + '\''
-            + ", emailI='" + EXPECTED_EMAIL + '\''
-            + '}', testI.toString());
+  /**
+   * Test de la méthode toString.
+   */
+  @Test
+  final void toStringTest() {
+    final Investisseur testI = new Investisseur(EXPECTED_CODE,
+            EXPECTED_NOM, EXPECTED_PRENOM, EXPECTED_EMAIL);
+    Assertions.assertEquals("Investisseur{" +
+              "codeI=" + EXPECTED_CODE +
+              ", nomI='" + EXPECTED_NOM + '\'' +
+              ", prenomI='" + EXPECTED_PRENOM + '\'' +
+              ", emailI='" + EXPECTED_EMAIL + '\'' +
+              '}', testI.toString());
   }
-
 }
